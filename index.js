2bb2ec36f6f572490b1fed75dff8a3c7cc694f2a
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-03-14 04:45:50 -0700

    sd

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/mongoDB");
-
 const Routes = require("./routes/mainRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
-
 const app = express();
-
 connectDB();
-
-app.use(cors({ origin: "*" }));
+const corsOptions = {
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type, Authorization",
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-
 app.use("/api", Routes);
 app.use("/pay", paymentRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
