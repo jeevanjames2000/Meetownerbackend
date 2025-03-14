f678ac5f24854b5dc517c53c462b67f3980f5e9d
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-03-14 05:32:41 -0700

    ssl

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+const https = require("https");
 const path = require("path");
 const connectDB = require("./config/mongoDB");
 const Routes = require("./routes/mainRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 const app = express();
+const privateKey = fs.readFileSync(
+  path.join(__dirname, "ssl", "private-key.pem"),
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  path.join(__dirname, "ssl", "certificate.pem"),
+  "utf8"
+);
+const credentials = { key: privateKey, cert: certificate };
 connectDB();
-const corsOptions = {
-  origin: "*",
-  methods: "GET,POST,PUT,DELETE,OPTIONS",
-  allowedHeaders: "Content-Type, Authorization",
-};
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders:
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api", Routes);
 app.use("/pay", paymentRoutes);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5001;
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(PORT, () => {
+  console.log(`🚀 HTTPS Server is running on port ${PORT}`);
+});
